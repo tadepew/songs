f29dc2dee6f6b0900d101d8d2985525e812b7d5a
Tristan Depew <tristan.depew@gmail.com>
2020-02-13 13:01:38 -0500

    inital steps

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  const songs = [{ id: 1, name: "Final Countdown" }];
+
+  res.status(200).json(songs);
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n** Running **\n`));
